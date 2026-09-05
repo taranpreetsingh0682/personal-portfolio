570a4300e5f059d42907a9888ba2256bdf7ccafe
taran-ai <taranpreetsingh62006@gmail.com>
2026-09-06 00:14:11 +0530

    Update portfolio update resume link

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -212,7 +212,7 @@ const About = () => {
               {/* Download Resume */}
               <div className='w-fit rounded-xl border border-red-100 bg-orange-200 px-3 py-2 transition-all duration-300 hover:-translate-y-1 hover:border-indigo-300'>
 
-                <a href='/resume.pdf' download>
+                <a href='/Cv.pdf' download>
                   Download Resume
                 </a>
 
